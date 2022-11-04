174816424ecf06dc8980ae8241f327a381f66914
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2022-11-04 13:14:46 +0530

    assignment table functions completed

--- model/assignments.db.php
+++ model/assignments.db.php
@@ -3,7 +3,7 @@
 function get_assignments($courseID){
     global $db;
     if($courseID){
-        $query  = 'SELECT A.ID, A.Description, C.coursename FROM assignments A LEFT JOIN courses C ON A.courseID = C.courseID WHERE A.courseID = :courseID ORDER BY A.ID';
+        $query  = 'SELECT A.ID, A.Description, C.coursename FROM assignments A LEFT JOIN courses C ON A.CourseID = C.courseID WHERE A.courseID = :courseID ORDER BY A.ID';
     }else{
         $query  = 'SELECT A.ID, A.Description, C.coursename FROM assignments A LEFT JOIN courses C ON A.courseID = C.courseID ORDER BY C.courseID';
     }
@@ -21,4 +21,26 @@ function get_assignments($courseID){
     
     $statement->closeCursor();
     return $assignments;
+}
+
+function delete_assignment($assignID){
+    global $db;
+
+    $query = 'DELETE FROM assignments WHERE ID = :assignID';
+    
+    $statement = $db->prepare($query);
+    $statement->bindValue(':assignID',$assignID);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function add_assignment($courseID,$description){
+    global $db;
+
+    $query = 'INSERT INTO assignments (Description,CourseID VALUES(:description,:courseID)';
+    
+    $statement = $db->prepare($query);
+    $statement->bindValue(':description',':courseID',$description,$courseID);
+    $statement->execute();
+    $statement->closeCursor();
 }
