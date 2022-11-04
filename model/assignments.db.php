deb8e500c4278520b5d9821a91d0587db752a1b6
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2022-11-04 12:55:33 +0530

    getassignments function complete

--- model/assignments.db.php
+++ model/assignments.db.php
@@ -8,6 +8,17 @@ function get_assignments($courseID){
         $query  = 'SELECT A.ID, A.Description, C.coursename FROM assignments A LEFT JOIN courses C ON A.courseID = C.courseID ORDER BY C.courseID';
     }
 
-    $statement = $db
+    $statement = $db->prepare($query);
+    
+    // Binds a value to a corresponding named or question mark placeholder in the SQL statement that was used to prepare the statement.
+    // value -> The value to bind to the parameter
 
+    $statement->bindValue(':courseID',$courseID);
+    $statement->execute();
+    $assignments = $statement->fetchAll();
+
+    // PDOStatement::closeCursor() frees up the connection to the server so that other SQL statements may be issued, but leaves the statement in a state that enables it to be executed again.
+    
+    $statement->closeCursor();
+    return $assignments;
 }
--- model/database.php
+++ model/database.php
@@ -4,6 +4,12 @@ $dsn = 'mysql:host=localhost;dbname=assignment_tracker';
 $username = 'root';
 // $password = 'stil not give a password;
 
+// Both MySQLi and PDO have their advantages:
+
+// PDO will work on 12 different database systems, whereas MySQLi will only work with MySQL databases.
+
+// oth are object-oriented, but MySQLi also offers a procedural API.
+
 try{
     $db = new PDO($dsn, $username);
 }catch(PDOException $e){
